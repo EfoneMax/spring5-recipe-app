ab2554e53e685bcd8bed78a69344d9d4e72ae250
glushkov <glushkov@proitr.ru>
2020-07-10 19:27:36 +0500

    200 done

--- src/main/java/efone/max/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/efone/max/spring5recipeapp/controllers/RecipeController.java
@@ -46,4 +46,13 @@ public class RecipeController {
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
+
+    @GetMapping("/{id}/delete")
+    public String deleteById(@PathVariable String id){
+
+       // log.debug("Deleting id: " + id);
+
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
+    }
 }
