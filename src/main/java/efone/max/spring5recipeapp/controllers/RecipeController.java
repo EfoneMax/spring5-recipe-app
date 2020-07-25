282fb6287ff55e0a8e1f1d933e61dee3ebe7daf3
glushkov <glushkov@proitr.ru>
2020-07-25 10:44:07 +0500

    227 done

--- src/main/java/efone/max/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/efone/max/spring5recipeapp/controllers/RecipeController.java
@@ -1,15 +1,22 @@
 package efone.max.spring5recipeapp.controllers;
 
 import efone.max.spring5recipeapp.commands.RecipeCommand;
+import efone.max.spring5recipeapp.exceptions.NotFoundException;
 import efone.max.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping("/recipe")
 public class RecipeController {
@@ -54,5 +61,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
